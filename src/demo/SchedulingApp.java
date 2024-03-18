cf55ec871dd18940afbbe52764489aa4e677eed3
Hajdusz2 <55916611+Hajdusz2@users.noreply.github.com>
2024-03-18 10:42:49 +0100

    Comments for MeetingManager
    Added javadoc for MeetingManager

--- src/demo/SchedulingApp.java
+++ src/demo/SchedulingApp.java
@@ -9,7 +9,7 @@ package demo;
 public class SchedulingApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("");
 	}
 
 }
--- src/managers/MeetingManager.java
+++ src/managers/MeetingManager.java
@@ -12,10 +12,19 @@ import dataaccess.ParticipantDAO;
 import models.Meeting;
 import models.Participant;
 
+/*
+ * This class is used to perform tasks relating to meetings.
+ */
 public class MeetingManager {
-	MeetingDAO meetingDAO = new MeetingDAO();
-	ParticipantDAO participantDAO = new ParticipantDAO();
-	
+	private MeetingDAO meetingDAO = new MeetingDAO();
+	private ParticipantDAO participantDAO = new ParticipantDAO();
+
+	/**
+	 * Creates a new meeting.
+	 * 
+	 * @param emails   Comma-separated string of participant emails.
+	 * @param datetime String representation of the meeting date and time.
+	 */
 	public void createMeeting(String emails, String datetime) {
 		List<String> listemails = new ArrayList<String>(Arrays.asList(emails.split(",")));
 
@@ -32,8 +41,14 @@ public class MeetingManager {
 			participantDAO.addParticipant(new Participant(email, meetingId));
 		}
 	}
-	
-	public List<Meeting> showSchedule (String email) {
+
+	/**
+	 * Shows the schedule of meetings for a given person.
+	 * 
+	 * @param email The email of the participant whose schedule is requested.
+	 * @return A list of Meetings that the person is involved in.
+	 */
+	public List<Meeting> showSchedule(String email) {
 		List<Participant> schedule = participantDAO.getSchedule(email);
 		List<Meeting> meetings = new ArrayList<Meeting>();
 		for (Participant participant : schedule) {
@@ -41,18 +56,27 @@ public class MeetingManager {
 		}
 		return meetings;
 	}
-	
-	public LocalDateTime suggestTime (String emails, String date) throws Exception {
+
+	/**
+	 * Suggests a time for the meeting based on the availability of the
+	 * participants.
+	 * 
+	 * @param emails Comma-separated string of participant emails.
+	 * @param date   String representation of the desired meeting date.
+	 * @return A LocalDateTime object representing the suggested meeting time.
+	 * @throws Exception if no time is available for the meeting.
+	 */
+	public LocalDateTime suggestTime(String emails, String date) throws Exception {
 		List<String> listemails = new ArrayList<String>(Arrays.asList(emails.split(",")));
 		List<Meeting> meetings = new ArrayList<Meeting>();
 		for (String email : listemails) {
 			meetings.addAll(showSchedule(email));
 		}
-		for (int i=8; i<16; i++) {
+		for (int i = 8; i < 16; i++) {
 			Boolean free = true;
 			for (Meeting meeting : meetings) {
 				if (meeting.getTimeSlot().getHour() == i) {
-					free =false;
+					free = false;
 				}
 			}
 			if (free) {
